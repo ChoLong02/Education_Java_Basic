f1d10302755008aa43a322918e4f39d4a0006967
Chulwoong Choi <sentilemon02@gmail.com>
2019-12-30 17:08:29 +0900

    Add files via upload

--- BasicJava/src/problem/bank/BankDAO.java
+++ BasicJava/src/problem/bank/BankDAO.java
@@ -1,5 +1,98 @@
 package problem.bank;
 
-public class BankDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import mybatis.SqlMapConfig;
+
+public class BankDAO {
+	// MyBatis 세팅값 호출
+	// Session을 생성하는 공장을 만드는 과정
+	SqlSessionFactory sqlSessionFactory = SqlMapConfig.getSqlSession();
+	
+	// mapper에 접근하기 위한 SqlSession
+	SqlSession sqlSession;
+	
+	List<BankDTO> list2;
+	int result;
+	
+	// 신규계좌 개설
+	public void insertBank(String bname, String pw) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		
+		try {
+			BankDTO bDto = new BankDTO(bname, pw);
+			result = sqlSession.insert("insertBank", bDto);
+			// sqlSession.commit();
+			if(result > 0) {
+				System.out.println("■■ " + bname + "님 신규계좌를 개설하였습니다.");
+			} else {
+				System.out.println("■■ 계좌개설에 실패하였습니다. 관리자에게 문의해주세요.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	
+	// 계좌 전체 조회
+	public void selectBank() {
+		sqlSession = sqlSessionFactory.openSession();
+		
+		try {
+			list2 = sqlSession.selectList("selBank");
+			
+			for (BankDTO line : list2) {
+				System.out.println(line.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public void selectAccount(int bno, String pw) {
+		sqlSession = sqlSessionFactory.openSession();
+		try {
+			BankDTO bDto = new BankDTO(bno, pw);
+			bDto = sqlSession.selectOne("selectAccount", bDto);
+			
+			if(bDto == null) {
+				System.out.println("■■ 존재하지 않는 계좌번호이거나 암호가 틀렸습니다.");
+				return;
+			} else {
+				System.out.println("■■ "+bno+"계좌의 총 금액은 " + bDto.getMoney()+" 입니다.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public void searchAccount(int size) {
+		sqlSession = sqlSessionFactory.openSession();
+		try {
+			BankDTO bDto = sqlSession.selectOne("searchAccount", size);
+			
+			System.out.println(bDto.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
 }
+
+
+
+
+
+
+
+
--- BasicJava/src/problem/bank/BankMain.java
+++ BasicJava/src/problem/bank/BankMain.java
@@ -13,9 +13,10 @@ public class BankMain {
 		System.out.println("■■ 1. 계좌 개설");
 		System.out.println("■■ 2. 입금");
 		System.out.println("■■ 3. 출금");
-		System.out.println("■■ 4. 계좌조회");
-		System.out.println("■■ 5. 사용자 검색");
-		System.out.println("■■ 6. 프로그램 종료");
+		System.out.println("■■ 4. 고객조회");
+		System.out.println("■■ 5. 계좌조회");
+		System.out.println("■■ 6. 사용자 검색");
+		System.out.println("■■ 7. 프로그램 종료");
 		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
 		System.out.print("■■ 번호>>");
 		code = sc.nextInt();
@@ -25,5 +26,43 @@ public class BankMain {
 		// 4.계좌 조회 만들기(SELECT, 전체조회)
 		// 5.사용자 검색 만들기(이름으로)
 		// 6.프로그램 종료기능 만들기
+		if(code == 1) {
+			System.out.println("■■ 신규계좌를 개설합니다. 값을 입력해주세요.");
+			System.out.print("■■ 계좌주>>");
+			sc.nextLine();
+			String bname = sc.nextLine();
+			System.out.print("■■ 패스워드>>");
+			String pw = sc.nextLine();
+			
+			bDao.insertBank(bname, pw);
+			
+			
+		} else if(code == 4) {
+			bDao.selectBank();
+		} else if(code == 5) { // 계좌 조회(1건)
+			// 계좌번호, 패스워드
+			System.out.println("■■ 계좌를 조회합니다. 계좌번호와 암호를 입력해주세요.");
+			System.out.print("■■ 계좌번호>");
+			int bno = sc.nextInt();
+			System.out.print("■■ 패스워드>");
+			sc.nextLine();
+			String pw = sc.nextLine();
+			
+			bDao.selectAccount(bno, pw);
+		} else if(code == 6) {
+			
+			bDao.searchAccount(101);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
