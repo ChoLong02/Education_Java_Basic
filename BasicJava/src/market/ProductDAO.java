0df9b469987841affb2e32f6e0c396bf5aa9350d
Chulwoong Choi <sentilemon02@gmail.com>
2020-01-06 14:03:53 +0900

    Add files via upload

---
+++ BasicJava/src/market/ProductDAO.java
@@ -0,0 +1,155 @@
+package market;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import mybatis.SqlMapConfig;
+
+public class ProductDAO {
+	SqlSessionFactory sqlSessionFactory = SqlMapConfig.getSqlSession();
+	SqlSession sqlSession;
+	int result;
+	List<ProductDTO> list;
+	Boolean flag; // default: false
+	
+	// 제품 등록&추가 기능 작동시 기존에 등록된 제품인지 최초입고제품인지 판별하는 기능
+	public boolean pdtAlready(String pname) {
+		sqlSession = sqlSessionFactory.openSession();
+		try {
+			result = sqlSession.selectOne("pdt.already", pname);
+			if(result > 0) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return flag;
+	}
+	
+	// 기존에 등록된 제품 수량 추가
+	public void cntPlusPdt(String pname, int cnt) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("pname", pname);
+		map.put("cnt", cnt);
+		map.put("flag", "plus");
+		try {
+			result = sqlSession.update("pdt.cntchange", map);
+			if(result > 0) {
+				System.out.println("▧▨ \""+pname+"\" 제품의 수량을 " + cnt +"개 추가하였습니다.");
+			} else {
+				System.out.println("▧▨  [Msg] Error, Contact your admin.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	// 신규상품 입고 등록
+	public void insertPdt(ProductDTO pDto) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		try {
+			result = sqlSession.insert("pdt.insert", pDto);
+			if(result > 0) {
+				System.out.println("▧▨  \""+pDto.getPname()+"\"제품"+pDto.getCnt()+"개를 신규 등록하였습니다.");
+			} else {
+				System.out.println("▧▨  [Msg] Error, Contact your admin.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	// 제품 삭제
+	public void deletePdt(String pname) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		try {
+			result = sqlSession.delete("pdt.delete", pname);
+			if(result > 0) {
+				System.out.println("▧▨  \""+pname+"\"제품을 삭제하였습니다.");
+			} else {
+				System.out.println("▧▨  [Msg] Error, Contact your admin.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	// 제품 전체 조회
+	public void selectPdt() {
+		sqlSession = sqlSessionFactory.openSession();
+		try {
+			list = sqlSession.selectList("pdt.select");
+			printList(list);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	// 제품 전체조외(재고가 > 1)
+	public List<ProductDTO> selectUsePdt() {
+		sqlSession = sqlSessionFactory.openSession();
+		try {
+			list = sqlSession.selectList("pdt.selectUsePdt");
+			printList(list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return list;
+	}
+	
+	// 상품 판매시 재고를 마이너스하는 함수
+	public void cntMinusPdt(String pname, int cnt) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("pname", pname);
+		map.put("cnt", cnt);
+		map.put("flag", "minus");
+		
+		try {
+			sqlSession.update("pdt.cntchange", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	// 출력
+	public void printList(List<ProductDTO> list) {
+		int i = 1;
+		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+		System.out.println("▒▒ 번호 \t 제품번호 \t 제품명 \t 제조사 \t 가격 \t 재고 \t 입고일자 ▒▒");
+		System.out.println("▒▒==================================================================================");
+		for (ProductDTO line : list) {
+			System.out.println("▒▒ "+ i +"\t"+ line.toString());
+			i += 1;
+		}
+		System.out.println("▒▒==================================================================================");
+		System.out.println("▒▒ 제품은 총 "+list.size()+"개 입니다.");
+		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+	}
+}
+
+
+
+
+
+
+
---
+++ BasicJava/src/market/SaleDAO.java
@@ -0,0 +1,70 @@
+package market;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import mybatis.SqlMapConfig;
+
+// 판매 관련 DAO 클래스
+public class SaleDAO {
+	SqlSessionFactory sqlSessionFactory = SqlMapConfig.getSqlSession();
+	SqlSession sqlSession;
+	int result;
+	List<SaleDTO> list;
+	
+	public int insertSale(HashMap<String, Object> map) {
+		sqlSession = sqlSessionFactory.openSession(true);
+		try {
+			result = sqlSession.insert("sale.insert", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return result;
+	}
+	
+	// 일일 판매량 출력하는 함수
+	public void dashBoard() {
+		sqlSession = sqlSessionFactory.openSession();
+		int cnt = 0;
+		int price = 0;
+		try {
+			list = sqlSession.selectList("sale.dashBoard");
+			int i = 0;
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+			System.out.println("▒▒ 번호 \t 제품명 \t 판매수량 \t 가격 ▒▒");
+			System.out.println("▒▒==================================================================================");
+			for (SaleDTO line : list) {
+				System.out.print("▒▒ "+ (i+1) +"\t");
+				System.out.print(line.getSname() + "\t");
+				System.out.print(line.getCnt() + "\t");
+				System.out.print(line.getTprice() + "\t");
+				System.out.println();
+				cnt += line.getCnt();
+				price += line.getTprice();
+				i += 1;
+			}
+			System.out.println("▒▒==================================================================================");
+			System.out.print("▒▒ 오늘 판매한 제품은 총 "+list.size()+"종류, ");
+			System.out.println("총 "+cnt+"개, 판매액 "+price+"원 입니다.");
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
