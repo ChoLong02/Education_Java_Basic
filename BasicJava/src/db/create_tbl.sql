267e9557d04d99bfb735a491c15821aa640526ae
Chulwoong Choi <sentilemon02@gmail.com>
2024-07-16 01:20:22 +0900

    Update create_tbl.sql

--- BasicJava/src/db/create_tbl.sql
+++ BasicJava/src/db/create_tbl.sql
@@ -10,11 +10,11 @@ create table tbl_study
 create table tbl_product
 ( 
     pno       INT auto_increment primary key,
-	 pname     VARCHAR(50) NOT NULL,
-	 company  VARCHAR(50) NOT NULL,
-	 price    INT NOT NULL,
-	 cnt    INT NOT NULL,
-	 regdate   datetime default current_timestamp() null
+    pname     VARCHAR(50) NOT NULL,
+    company  VARCHAR(50) NOT NULL,
+    price    INT NOT NULL,
+    cnt    INT NOT NULL,
+    regdate   datetime default current_timestamp() null
 );
 
 
@@ -22,8 +22,8 @@ create table tbl_product
 create table tbl_sale
 ( 
     sno       INT auto_increment primary key,
-	 sname     VARCHAR(50) NOT NULL,
-	 cnt    INT NOT NULL,
-	 tprice    INT NOT NULL,
-	 date   datetime default current_timestamp() null
+    sname     VARCHAR(50) NOT NULL,
+    cnt    INT NOT NULL,
+    tprice    INT NOT NULL,
+    rgdate   datetime default current_timestamp() null
 );
